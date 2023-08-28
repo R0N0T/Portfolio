32a9341e7252c69d4329e02dc6b67e74268f338f
Rohit Singh <71979637+RON0T@users.noreply.github.com>
2023-08-28 22:43:10 +0530

    added about me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import Navbar from './components/navbar';
 import ContactMe from './components/contactMe';
+import AboutMe from './components/AboutMe';
 function App() {
   return (
     <div>
+      <AboutMe />
       <Navbar />
       <ContactMe/>
     </div>
---
+++ src/components/AboutMe.js
@@ -0,0 +1,19 @@
+// AboutMe.js
+import React from 'react';
+import './styles.css'; // Import your CSS file
+
+function AboutMe() {
+  return (
+    <div className="about-me-container">
+      <h1>About Me</h1>
+      <p>
+        Welcome to my portfolio! I am a passionate web developer with expertise in React.js,
+        HTML, CSS, and more. I love creating user-friendly and visually appealing web
+        experiences.
+      </p>
+      {/* Add more content about yourself */}
+    </div>
+  );
+}
+
+export default AboutMe;
